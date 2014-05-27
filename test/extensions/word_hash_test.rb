93b1d633a2c104760efcfda38aad89ae93144c8e
Egor Vakhromtsev <e.vakhromtsev@mediacrat.ru>
2014-05-27 16:06:40 +0400

    Use Array#sum_with_identity instead of Array#sum in tests

--- test/extensions/word_hash_test.rb
+++ test/extensions/word_hash_test.rb
@@ -17,19 +17,19 @@ end
 class ArrayExtensionsTest < Test::Unit::TestCase
 
   def test_plays_nicely_with_any_array
-    assert_equal [Array].sum, Array
+    assert_equal [Array].sum_with_identity, Array
   end
 
   def test_monkey_path_array_sum
-    assert_equal [1,2,3].sum, 6
+    assert_equal [1,2,3].sum_with_identity, 6
   end
 
   def test_summing_an_empty_array
-    assert_equal [nil].sum, 0
+    assert_equal [nil].sum_with_identity, 0
   end
 
   def test_summing_an_empty_array
-    assert_equal Array[].sum, 0
+    assert_equal Array[].sum_with_identity, 0
   end
 
 end
